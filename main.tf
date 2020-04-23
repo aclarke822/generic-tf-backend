a14ae7fc21a41605110146bf14be572bd6d3669c
Austin Clarke <aclarke822@gmail.com>
2020-04-22 22:03:14 -0500

    add s3 backend bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,29 @@ provider "aws" {
   region  = "us-east-2"
   version = "~> 2.8"
 }
+
+resource "aws_s3_bucket" "backend" {
+  bucket = "terraform.austinclarke.net"
+  acl    = "private"
+  force_destroy               = false
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "TF Backend"
+  }
+}
