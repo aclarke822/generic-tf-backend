3ac37494aadf01ec01adb064cd6cf49024f7677a
Austin Clarke <aclarke822@gmail.com>
2020-04-22 22:04:16 -0500

    make bucket name generic

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "backend" {
-  bucket = "terraform.austinclarke.net"
+  bucket = "bucket.name.here"
   acl    = "private"
   force_destroy               = false
 
